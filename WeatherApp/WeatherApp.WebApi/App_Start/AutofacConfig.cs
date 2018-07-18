5037157f422b811daec038a47ded036ded3464da
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2018-07-18 21:14:49 +0200

    refractor

---
+++ WeatherApp/WeatherApp.WebApi/App_Start/AutofacConfig.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using System.Web.Http;
+using Weather.Core.Repositories.Implementations;
+using Weather.Core.Repositories.IRepositories;
+using Weather.Core.Services;
+using Weather.Core.Services.Infrastructure;
+
+namespace WeatherApp.WebApi
+{
+    public class AutofacConfig
+    {
+       // private static IContainer Container { get; set; }
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            //Register your Web API controllers.  
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<WeatherRepository>()
+                   .As<IWeatherRepository>()
+                   .InstancePerRequest();
+            builder.RegisterType<WeatherService>()
+                  .As<IWeatherService>()
+                  .InstancePerRequest();
+            Container = builder.Build();
+
+            return Container;
+        }
+
+    }
+}
---
+++ WeatherApp/WeatherApp.WebApi/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+
+
+namespace WeatherApp.WebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AutofacConfig.Initialize(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+    }
+}
