2690b4672efa3d42190f986b4c5832462d8d3c41
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2018-07-19 15:42:11 +0200

    added tests

---
+++ WeatherApp/WeatherApp.BDDTests/WeatherTests.cs
@@ -0,0 +1,31 @@
+﻿using WeatherApp.FrontEnd.Controllers;
+using WeatherApp.FrontEnd.Models;
+using WeatherApp.IntegrationTests;
+using Xunit;
+
+namespace WeatherApp.BDDTests
+{
+    public class WeatherTests
+    {
+        public WeatherTests()
+        {
+            MvcAppConfig.SetupTestRun();
+        }
+        [Theory]
+        [InlineData("Poland", "Warsaw")]
+        [InlineData("Poland", "Gdansk")]
+        [InlineData("Germany", "Berlin")]
+        public void CurrentWeatherBDDTests(string country,string city)
+        {
+            MvcAppConfig.app.NavigateTo<HomeController>(controller => controller.Index());
+            MvcAppConfig.app.FindFormFor<GetCurrentWeather>()
+                .Field(m => m.Country).SetValueTo(country)
+                .Field(m => m.City).SetValueTo(city)
+                .Submit();
+            var cityReturned = MvcAppConfig.app.Browser.FindElementById("td-city").Text;
+            var countryReturned = MvcAppConfig.app.Browser.FindElementById("td-country").Text;
+            Assert.Equal(country, countryReturned);
+            Assert.Equal(city, cityReturned);
+        }
+    }
+}
--- WeatherApp/WeatherApp.IntegrationTests/MvcAppConfig.cs
+++ WeatherApp/WeatherApp.IntegrationTests/MvcAppConfig.cs
@@ -1,14 +1,8 @@
 ﻿using Microsoft.Build.Utilities;
 using SpecsFor.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Routing;
 using WeatherApp.FrontEnd;
-using WeatherApp.WebApi;
+
 
 namespace WeatherApp.IntegrationTests
 {
@@ -18,9 +12,7 @@ namespace WeatherApp.IntegrationTests
         public const string APIUrl = "http://localhost:51583/";
         public static SpecsForIntegrationHost _host;
         private static bool IsRun = false;
-        private static bool IsApiRun = false;
         public static MvcWebApp app;
-        public static MvcWebApp appApi;
         public static void SetupTestRun()
         {
             if (IsRun)
@@ -31,7 +23,8 @@ namespace WeatherApp.IntegrationTests
             .With(Project.Named("WeatherApp.FrontEnd"))
             .UseMSBuildExecutableAt(msBuild);
             config.BuildRoutesUsing(r => RouteConfig.RegisterRoutes(r));
-            config.UseBrowser(BrowserDriver.InternetExplorer);  
+            config.UseBrowser(BrowserDriver.InternetExplorer);
+            config.PostOperationDelay(new TimeSpan(0,0,0,4));
             _host = new SpecsForIntegrationHost(config);
             _host.Start();
             app = new MvcWebApp();
@@ -45,67 +38,5 @@ namespace WeatherApp.IntegrationTests
             IsRun = false;
         }
 
-        public static void SetupApiTestRun()
-        {
-            if (IsRun)
-                return;
-            string msBuild = ToolLocationHelper.GetPathToBuildToolsFile("msbuild.exe", "16.0");
-            var config = new SpecsForMvcConfig();
-            config.UseIISExpress()
-            .With(Project.Named("WeatherApp.WebApi"))
-            .UseMSBuildExecutableAt(msBuild);
-            //config.BuildRoutesUsing(r => WebApiConfig.Register(r));
-            config.UseBrowser(BrowserDriver.InternetExplorer);
-            _host = new SpecsForIntegrationHost(config);
-            _host.Start();
-            appApi = new MvcWebApp();
-            //WebApiConfig.Register();
-            IsRun = true;
-        }
-
-        public static void DownApiTestRun()
-        {
-            _host.Shutdown();
-            IsRun = false;
-        }
-
-        public static void AddHttpRoutes(this HttpRouteCollection routeCollection)
-        {
-            var routes = GetRoutes();
-            routes.ForEach(route => routeCollection.MapHttpRoute(route.Name, route.Template, route.Defaults));
-        }
-
-        public static void AddHttpRoutes(this RouteCollection routeCollection)
-        {
-            var routes = GetRoutes();
-            routes.ForEach(route => routeCollection.MapHttpRoute(route.Name, route.Template, route.Defaults));
-        }
-
-        private static List<Route> GetRoutes()
-        {
-            return new List<Route>
-               {
-                   new Route(
-                       "DefaultApi",
-                       "api/{controller}/{id}",
-                       new { id = RouteParameter.Optional })
-               };
-        }
-
-        private class Route
-        {
-            public Route(string name, string template, object defaults)
-            {
-                this.Name = name;
-                this.Template = template;
-                this.Defaults = defaults;
-            }
-
-            public object Defaults { get; set; }
-
-            public string Name { get; set; }
-
-            public string Template { get; set; }
-        }
     }
 }
